e4bd5fc61b915da59b27706e02dc06e79bf0b1d0
rajeshbd99 <rajesh99.bd@gmail.com>
2024-10-14 19:33:23 +0530

    Payoo Login Done

---
+++ Banking_system/js/login.js
@@ -0,0 +1,12 @@
+document.getElementById('button-login').addEventListener('click',function(event){
+    event.preventDefault();
+    const phoneNumber = document.getElementById('phone-number').value;
+    const pinNumber = document.getElementById('pin-number').value;
+    
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        window.location.href = 'home.html';
+    }
+    else{
+        console.log('Login Failed');
+    }
+})
