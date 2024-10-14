2f0bbaba70a92f3a48998e512945fa216701fe85
rajeshbd99 <rajesh99.bd@gmail.com>
2024-10-14 20:07:38 +0530

    Payoo Add Money Done

--- Banking_system/js/home.js
+++ Banking_system/js/home.js
@@ -1,1 +1,21 @@
-console.log('Home Page');
+document.getElementById('btn-add-money').addEventListener('click',function(event){
+    //prevent page from reloading
+    event.preventDefault();
+    
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    const pinNumberInput = document.getElementById('input-pin-nuber').value;
+    
+
+    if(pinNumberInput === '1234'){
+        const balance = document.getElementById('account-balance').innerText;
+        
+        const newBalance = parseFloat(balance) + parseFloat(addMoneyInput);
+
+        document.getElementById('account-balance').innerText = newBalance; 
+        
+    }
+    else{
+        console.log('Invalid Pin');
+    }
+
+})
